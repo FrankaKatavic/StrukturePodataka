b0a7369b2f09a3dd32758de79cac3edcf2bc838f
AntonijaKezic <87046226+AntonijaKezic@users.noreply.github.com>
2024-10-29 21:11:51 +0100

    Update Vjezbe3.c

--- Vjezbe 3/Vjezbe3.c
+++ Vjezbe 3/Vjezbe3.c
@@ -36,7 +36,7 @@ int PrependList(position head, char* name, char* lastName, int by) {
 	newPerson = CreatePerson(name, lastName, by);
 	if (!newPerson) {
 		printf("Failed in person creation!\n");
-		return NULL;
+		return 0;
 	}
 
 	newPerson->next = head->next;
@@ -48,34 +48,17 @@ int PrependList(position head, char* name, char* lastName, int by) {
 int printList(position first)
 {
 	position temp = NULL;
-	temp = first;
+	temp = first->next;
 
 	while (temp != NULL)
 	{
-		printf("%s %s %d", temp->name, temp->lastName, temp->birthyear);
+		printf("%s %s %d \n", temp->name, temp->lastName, temp->birthyear);
 		temp = temp->next;
 
 	}
 	return EXIT_SUCCESS;
 
 }
-
-int AppendList(position head, char* name, char* lastName, int by)
-{
-	position newPerson = NULL;
-	newPerson = CreatePerson(name, lastName, by);
-	position last = NULL;
-	if (!newPerson) {
-		printf("Failed in person creation!\n");
-		return NULL;
-	}
-
-	last = FindLast(head);
-	newPerson->next = last->next;
-	last->next = newPerson;
-	return EXIT_SUCCESS;
-}
-
 position FindLast(position head)
 {
 	position last = NULL;
@@ -87,6 +70,23 @@ position FindLast(position head)
 	}
 	return last;
 }
+
+int AppendList(position head, char* name, char* lastName, int by)
+{
+	position newPerson = NULL;
+	newPerson = CreatePerson(name, lastName, by);
+	position last = NULL;
+	if (!newPerson) {
+		printf("Failed in person creation!\n");
+		return 0;
+	}
+
+	last = FindLast(head);
+	newPerson->next = last->next;
+	last->next = newPerson;
+	return EXIT_SUCCESS;
+}
+
 
 position FindByLastName(position first, char* lastName)
 {
@@ -100,6 +100,18 @@ position FindByLastName(position first, char* lastName)
 		}
 		temp = temp->next;
 	}
+	return NULL;
+}
+position FindPrev(position head, position wanted) {
+	position temp = head;
+
+	while (temp->next != NULL) {
+		if (temp->next == wanted) {
+			return temp;
+		}
+		temp = temp->next;
+	}
+
 	return NULL;
 }
 int Delete(position head, position name) {
@@ -116,47 +128,134 @@ int Delete(position head, position name) {
 
 }
 
-position FindPrev(position head, position wanted) {
-	position temp = head;
 
-	while (temp->next != NULL) {
-		if (temp->next == wanted) {
-			return temp;
-		}
-		temp = temp->next;
-	}
-
-	return NULL;
-}
 
 int insertBehind(position head, char* lastname, char* nameNew, char* lastNameNew, int by)
 {
-	position newPerson = NULL;
 	position newPerson = NULL;
 	newPerson = CreatePerson(nameNew, lastNameNew, by);
 	position prev = NULL;
 	if (!newPerson) {
 		printf("Failed in person creation!\n");
-		return NULL;
+		return 0;
 	}
-	prev = FindByLastName(head, *lastname);
+	prev = FindByLastName(head, lastname);
 	if (prev != NULL)
 	{
-		
+		newPerson->next = prev->next;
+		prev->next = newPerson;
 	}
+	return EXIT_SUCCESS;
 }
 
+int insertBefore(position head, char* lastname, char* nameNew, char* lastNameNew, int by)
+{
+	position newPerson = NULL;
+	newPerson = CreatePerson(nameNew, lastNameNew, by);
+	position prev = NULL;
+	position behind = NULL;
+	if (!newPerson) {
+		printf("Failed in person creation!\n");
+		return 0;
+	}
+	behind = FindByLastName(head, lastname);
+	prev = FindPrev(head, behind);
+	if (prev != NULL)
+	{
+		newPerson->next = prev->next;
+		prev->next = newPerson;
+	}
+	return EXIT_SUCCESS;
+}
+int unos(position head)
+{
+	position x = head->next;
+	FILE* file = fopen("popis.txt", "w");
+	if (!file) {
+		printf("Ne mogu otvoriti datoteku.\n");
+		return 1;
+	}
 
+	while (x != NULL)
+	{
+		fprintf(file, "%s %s %d\n", x->name, x->lastName, x->birthyear);
+		x = x->next;
+	}
+	
 
+	fclose(file);
+	return EXIT_SUCCESS;
+}
+
+int citanje(position head)
+{
+	FILE* file = fopen("popis.txt", "r");
+	if (!file) {
+		printf("Ne mogu otvoriti datoteku.\n");
+		return 1;
+	}
+
+	char name[MAX_SIZE];
+	char lastName[MAX_SIZE];
+	int birthyear;
+
+	while (fscanf(file, "%s %s %d", name, lastName, &birthyear) == 3) {
+		AppendList(head, name, lastName, birthyear);
+	}
+
+	fclose(file);
+	return EXIT_SUCCESS;
+}
+void SortirajListu(position head) {
+	position p, prev, temp, end = NULL;
+	int swapped;
+
+	do {
+		swapped = 0;
+		p = head;
+
+		while (p->next != end && p->next->next != NULL) {
+			prev = p;
+			temp = p->next;
+
+			// Usporedimo prezimena; ako su prezimena ista, uspoređujemo imena
+			if (strcmp(temp->lastName, temp->next->lastName) > 0 ||
+				(strcmp(temp->lastName, temp->next->lastName) == 0 && strcmp(temp->name, temp->next->name) > 0)) {
+
+				prev->next = temp->next;
+				temp->next = temp->next->next;
+				prev->next->next = temp;
+
+				swapped = 1;
+			}
+
+			p = p->next;
+		}
+		end = p;
+	} while (swapped);
+}
+int DeleteAll(position head) {
+	position temp = head->next;
+	position toDelete = NULL;
+
+	while (temp != NULL) {
+		toDelete = temp;
+		temp = temp->next;
+		free(toDelete);
+	}
+
+	head->next = NULL;
+	return EXIT_SUCCESS;
+}
 int main()
 {
 	Person head = {
 		.name = "",
 		.lastName = "",
-		.birthyear = 0,
+		.birthyear = {0},
 		.next = NULL
 	};
-	/*
+	
 	// Testiranje funkcija
 	PrependList(&head, "Ana", "Kovac", 1990);
 	AppendList(&head, "Jana", "Peric", 1985);
@@ -173,7 +272,28 @@ int main()
 
 	printf("\nNova lista:\n");
 	printList(&head);
-	*/
+	
+	insertBehind(&head, "Horvat", "Lana", "Matic", 2004);
+	printList(&head);
+	printf("\n\n");
+	insertBefore(&head, "Matic", "Sara", "Lukenda", 2003);
+	printList(&head);
+
+	//unos(&head);
+	printf("\n\n");
+
+	printf("Lista nakon ucitavanja iz datoteke:\n");
+	citanje(&head);
+	printList(&head);
+	SortirajListu(&head);
+	printf("\n\n");
+	printList(&head);
+	
+	DeleteAll(&head);
+	printf("\n\n Bez liste:");
+	printList(&head);
+
+	
 	return 0;
 
 }
